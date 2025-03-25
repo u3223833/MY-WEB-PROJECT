fe6f206d712635977ecba03e6f0c99ffb1d3f3b9
ttocsecarg333 <112055166+u3223833@users.noreply.github.com>
2025-03-25 17:32:37 +1100

    Update dropdown.js

--- assets/css/js/dropdown.js
+++ assets/css/js/dropdown.js
@@ -1,1 +1,16 @@
+// assets/js/dropdown.js
+const projectsLink = document.getElementById('projectsLink');
+const projectsDropdown = document.getElementById('projectsDropdown');
 
+projectsLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  projectsDropdown.classList.toggle('show');
+});
+
+window.addEventListener('click', function(e) {
+  if (!e.target.matches('#projectsLink')) {
+    if (projectsDropdown.classList.contains('show')) {
+      projectsDropdown.classList.remove('show');
+    }
+  }
+});
